d6b80edb82cc19873f9373d67ba758fef4ddb5a3
Aleksandr <U4iha312@mail.ru>
2021-09-13 06:46:16 +0300

    add Angular

--- ProjectX.Angular/ClientApp/src/app/courses/courses.component.ts
+++ ProjectX.Angular/ClientApp/src/app/courses/courses.component.ts
@@ -1,6 +1,6 @@
 import { Component, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+// Вроде готово!
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html'
