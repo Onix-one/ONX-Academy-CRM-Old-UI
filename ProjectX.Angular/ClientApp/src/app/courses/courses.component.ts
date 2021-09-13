f728b4590b063296b80feea8fb64b2b85e54b97d
Aleksandr <U4iha312@mail.ru>
2021-09-13 09:22:55 +0300

    add Changes

--- ProjectX.Angular/ClientApp/src/app/courses/courses.component.ts
+++ ProjectX.Angular/ClientApp/src/app/courses/courses.component.ts
@@ -1,18 +1,33 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html'
 })
-export class CoursesComponent {
+export class CoursesComponent{
   public courses: ICourseDto[];
+  public coursesAll: ICourseDto[];
+  specializationId: number;
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
     http.get<ICourseDto[]>(baseUrl + 'courses').subscribe(result => {
       this.courses = result;
+      this.coursesAll = result;
     }, error => console.error(error));
   }
+
+  onSelectSpecialization(id){
+    this.specializationId = id;
+    if (this.specializationId === 0) {
+      this.courses = this.coursesAll;
+    }
+    else {
+      this.courses = this.coursesAll.filter(_ => _.specializationId === this.specializationId);
+    }
+  }
 }
+
 interface ICourseDto {
   id: number;
   title: string;
